0f27f80489ab969cbd6b22de88fb590921884249
Brian Birdwell <bbirdwell@gmail.com>
2018-10-06 22:16:15 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,10 +17,11 @@ class Transfer
   end
   
   def execute_transaction
-    if self.status == "pending"
+    if self.status == "pending" && self.valid? == true
       self.sender.balance - self.amount
       self.receiver.balance + self.amount
       self.status = "complete"
+    else "Transaction rejected. Please check your account balance."
     end
   end
     
